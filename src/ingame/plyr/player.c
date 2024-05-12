36478c83234beb082621c41108b5808786c1311b
kenshen112 <finalfantasytimothy@gmail.com>
2024-05-11 20:05:42 -0700

    Added player.c and plyr_mdl.c

---
+++ src/ingame/plyr/player.c
@@ -0,0 +1,295 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_002359C0);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_002359F0);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_00235A28);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_00235A60);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", InitPlayer__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReleasePlayer__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ClrEneSta__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrDebug__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerMainCmn__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrNormalCtrl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrMotionMovement__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerGameOver__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqPlyrDead__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrDead__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrMepachiCtrl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_00236530);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrBattleCheck__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrDamageCtrl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrAvoidCheck__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", LeverGachaChk__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", RTSpiritsDownModeOn__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", RTSpiritsDownModeOff__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqPlyrHPSPdownP__FP9PLCMN_WRKUs);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqPlyrHPdownP__FP9PLCMN_WRKUs);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqPlyrSPdownP__FP9PLCMN_WRKUs);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqPlyrHPSPdown__FP9PLCMN_WRKUs);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqPlyrHPdown__FP9PLCMN_WRKUs);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqPlyrSPdown__FP9PLCMN_WRKUs);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrSPdownCtrl__FP9PLCMN_WRK);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrHPdownCtrl__FP9PLCMN_WRK);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_002378A8);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqCamTraceNearEne__FPv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrFinderIn__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrFinder__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrFinderEnd__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrFinderEnd1__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrFinderEnd2__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrFinderQEnd__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrFinderIn__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrFinderEnd__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_00237FE8);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_00238090);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrNModeCtrl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrFModeMoveCtrl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrMovePad__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrMoveChk__FP8MOVE_BOXPff);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrMoveChkV__FP8MOVE_BOXPff);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrMovePadFind__FP8MOVE_BOXPf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetMovePad__FUc);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrMoveStaChk__Ff);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrLeverInputChk__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetMoveSpeed__FPf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrPosSet__FP8MOVE_BOXPf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrHeightCtrl__FPf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PadInfoTmpSave__FPUcUcPff);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", CngPlyrRotRapid__FP8MOVE_BOXf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_00239298);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_002393D0);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_00239610);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrFinderCtrl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", EneFrameHitChk__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", FModeScreenEffect__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", NearEneInfo__FP9PLCMN_WRK);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", NearAllEneInfo__FP9PLCMN_WRK);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrDWalkTmCtrl__FP9PLCMN_WRK);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrCamTurnChk__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_00239C60);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrPhotoChk2__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrVibCheck__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrVibCtrl__FUc);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrVibCtrlBig__FUcUc);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PhotoDmgChk2__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PhotoDmgChkSub2__FP7ENE_WRKiPi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_0023ABC8);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PhotoPointCulcEne2__FP7ENE_WRKP16BONUS_SHOT_SCOREP11SUBJECT_WRKP15_HINT_PHOTO_REQi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlayerPos__FPf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlayerFinderPos__FPfT0);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlayerPos__FPf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlayerRot__FPf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlayerFloor__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlayerHitRadius__Ff);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrAnime__FUcUc);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrNAnimeCtrl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlyrFloor__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlyrEquipmentFilmType__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerGetNowHPPercentage__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerGetNowMPPercentage__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlyrOldAreaNo__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlyrAreaNo__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlyrRoomID__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrAreaNo__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetPlyrRoomID__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", InFinderMode__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerModeIsFinder__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", InDamageState__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", IsPlayerInBattle__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", FrameInsideChk__FPfN20);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_0023C1C8);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_0023C7A8);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrFlashlight__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrSpotMoveCtrl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetEnePowerDegree__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ChkPhotoAble__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", CulcEP__FPfT0);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", CulcEP2__FPfT0);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", CulcEP3__FPf);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", InFinderFrameSub__FPfT0ff);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrRoomCheck__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetNearestDistFromPlyrToEnemy__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", DoorMotionIsEnd__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ReqPlyrDoorMotion__Fii);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrOpenDoor__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ClearPlyrDoorFlg__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlyrDoorFlg__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetPlyrDoorMoveFlg__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerChangeMode__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", MovePlyrStairs__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", CheckPlyrAnimeEnd__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrCondCheck__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrSEStop__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlyrOutsideCheck__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", playerSetFlashlightType__F20PLAYERFLASHLIGHTTYPE);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", playerGetFlashlightType__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", GetCenterPoint__FPfN20);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", ClearPlyrMoveStatus__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", IsFinderLocked__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerFinderLock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerFinderUnlock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerShutterLock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerShutterUnlock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerMoveLock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerMoveUnlock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerActionLock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerActionUnlock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerRunLock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerRunUnlock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerCurseLock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerCurseUnlock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerLock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", PlayerUnlock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", SetSave_PlyrWrk__FP12MC_SAVE_DATA);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", CheckPlayerNeckSW__FUc);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", Init__10CEneTracer);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", Work__10CEneTracer);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", Req__10CEneTracerii);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_0023E0A0);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/player", func_0023E0E0);
---
+++ src/ingame/plyr/plyr_mdl.c
@@ -0,0 +1,101 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", _fixed_array_assert__FPCcUiUi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", _fixed_array_verifyrange__H1ZPv_UiUi_PX01);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", func_0023E168);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", _fixed_array_verifyrange__H1ZPUi_UiUi_PX01);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlInit__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlResetReq__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", SetupPlyrMdl__Fiiii);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", IsReadyPlyrMdl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", ReleasePlyrMdl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", ReqPlayerMim__Fii);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", ReqPlayerMimContinue__Fii);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", StopPlayerMim__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", IsPlayerMimParts__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlSetSave__FP12MC_SAVE_DATA);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", GetPlyrFtype__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", ReqPlayerAnime__FUc);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", PlayerInterpFlame__FP8ANI_CTRLii);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlMotionWork__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlGetMATRIX__FPA3_fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", CalcGirlCoord__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", playerUseDoorLight__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", playerSetLight__FPCfPC35GRA3DEMULATIONLIGHTDATACREATIONDATA);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", _GetEmulationLightdataCreationDataRef__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", playerCalcAlpha__FPC8ANI_CTRL);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", playerDrawShadow__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", PlayerDrawLock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", PlayerDrawUnlock__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", DrawGirl__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", LeftHandCtrl__FP13SGDCOORDINATE);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlGetANI_CTRL__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlGetShadowANI_CTRL__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", PlyrNeckFrameInit__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", PlyrNeckInit__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", PlyrNeckMain__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", SetPlyrNeckFlg__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", PlyrNeckRegisterTarget__FP14_LOOK_AT_PARAM25_LOOK_TARGET_PRIORITY_MIO);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", PlyrNeckGetParam__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlBankPlay__FiiiiP11_SND_3D_SETii);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", plyr_mdlBankIsLoopSnd__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", GetPlyrMdlNo__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", SetPlyrMdlNo__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", GetPlyrAcsNo__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", SetPlyrAcsNo__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", GetSisterMdlNo__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", SetSisterMdlNo__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", GetSisterAcsNo__Fv);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", SetSisterAcsNo__Fi);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", CostumeSetSave__FP12MC_SAVE_DATA);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", func_00240050);
+
+INCLUDE_ASM("asm/nonmatchings/ingame/plyr/plyr_mdl", RoomInInfoInit__Fv);
