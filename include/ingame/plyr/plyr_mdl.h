8cfb393c517179c0a8986774bfc2ca6dc49d35c7
weirdbeardgame <finalfantasytimothy@gmail.com>
2024-06-12 10:59:51 -0700

    Fixed symbol name

--- include/ingame/plyr/plyr_mdl.h
+++ include/ingame/plyr/plyr_mdl.h
@@ -46,28 +46,7 @@ struct PLYR_PLYR_DATA : MAN_DATA
 
  public:
   PLYR_PLYR_DATA &operator=(const PLYR_PLYR_DATA &rval);
-  /* vtable[1] */ virtual int Setup(int param_1, int param_2, int param_3,
-                                    int param_4, int param_5)
-  {
-    int return_val = 0;
-    if (!plyr_req_other_mdl)
-    {
-      return_val = 1;
-      mmanageReqItemMdl(0);
-      mmanageReqItemMdl(1);
-      plyr_req_other_mdl = 1;
-    }
-    if (SetupIn(param_1, param_2, param_3, param_4, param_5))
-    {
-      if (plyr_init_ok)
-      {
-        ChrSortDelete(1);
-        plyr_init_ok = 0;
-      }
-      return_val = 1;
-    }
-    return return_val;
-  }
+  /* vtable[1] */ virtual int Setup(int param_1, int param_2, int param_3, int param_4, int param_5);
 
   /* vtable[2] */ virtual int IsReady()
   {
