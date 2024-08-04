66062d4953078e8cc0ce3ac330f66a09f4d8a0c5
weirdbeardgame <finalfantasytimothy@gmail.com>
2024-08-03 17:27:13 -0700

    Added ctor, dtor

---
+++ tools/splat_ext/dtor.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+import spimdisasm
+
+from ..common.data import CommonSegData
+from ...disassembler.disassembler_section import DisassemblerSection
+
+
+class PS2SegDtor(CommonSegData):
+    """Segment that only contains a pointer to C++ global data initialization functions"""
+
+    def get_linker_section(self) -> str:
+        return ".dtor"
+
+    def get_section_flags(self) -> Optional[str]:
+        return "a"
+
+    def configure_disassembler_section(self, disassembler_section: DisassemblerSection) -> None:
+        "Allows to configure the section before running the analysis on it"
+
+        super().configure_disassembler_section(disassembler_section)
+
+        section = disassembler_section.get_section()
+        assert isinstance(section, spimdisasm.mips.sections.SectionBase)
+
+        section.enableStringGuessing = False
+        section.typeForOwnedSymbols = "s32"
+        section.sizeForOwnedSymbols = 4
