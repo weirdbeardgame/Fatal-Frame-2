81aad23da6d010e1d6694bbcf9b820e8fd858e70
weirdbeardgame <obarrtimothy@gmail.com>
2025-04-23 15:14:09 -0700

    Added mc_title_dat.c Added FilesToSplit list WIP: Work on yaml

---
+++ src/system/mc/mc_title_dat.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+// *****************************************************************************
+// FILE -- /home/zero_rom/zero2np/src/system/mc/dat/mc_title_dat.c
+// *****************************************************************************
+
+/* data 31e3c8 */ char *game_dir_name[6];
+/* data 31e3e0 */ char *game_file_name[7];
+/* sdata 3f1b40 */ char *album1_file_name[1];
+/* sdata 3f1b48 */ char *album2_file_name[1];
+/* sdata 3f1b50 */ char *album3_file_name[1];
+/* sdata 3f1b58 */ char *album4_file_name[1];
+/* sdata 3f1b60 */ char *album5_file_name[1];
+/* rdata 3bbd60 */ char mc_icon_name[6][3][31];
+/* rdata 3bbf90 */ char mc_icon_title[29];
+/* rdata 3bbfb0 */ char mc_icon_sub_title[6][64];
+/* rdata 3bc130 */ int icon_data_label[6][3];
