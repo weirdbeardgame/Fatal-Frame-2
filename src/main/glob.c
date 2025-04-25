de2ad74026701e3b732e439eaedce5d0cbeb4386
weirdbeardgame <obarrtimothy@gmail.com>
2025-04-24 21:20:32 -0700

    Attempts at splitting glob files

---
+++ src/main/glob.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+/* data 316c80 */ //PAD_STRUCT pad[2];
+/* data 317000 */ short unsigned int *key_now[32];
+/* data 317080 */ short unsigned int *key_bak[32];
+/* data 317100 */ //SYS_WRK sys_wrk;
+/* data 317120 */ //OPTION_WRK opt_wrk;
+/* data 317130 */ //CAM_CUSTOM_WRK cam_custom_wrk;
+/* data 317140 */ //DEBUG_WRK debug_wrk;
